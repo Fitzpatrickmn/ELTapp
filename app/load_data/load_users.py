b29d0d9751717fb40581e99036ddbde9a70d6949
Michelle N. Fitzpatrick <michgithub99@gmail.com>
2024-09-05 01:52:06 -0400

    modularize load_users.py

--- app/load_data/load_users.py
+++ app/load_data/load_users.py
@@ -1,45 +1,44 @@
-import sqlite3
-import json
+from db_utils import load_json_data, connect_to_db, create_table, insert_data, query_data, close_connection
+
 
 # Load JSON data
-with open('app/output_data/users.json', 'r') as file:
-    data = json.load(file)
-
-# Connect to SQLite
-conn = sqlite3.connect('main.db')
-cursor = conn.cursor()
-
-cursor.execute('DROP TABLE IF EXISTS users')
+data = load_json_data('app/output_data/users.json')
 
 # Create table 
-cursor.execute('''
+create_users_table_sql = '''
     CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER, 
         user_name STRING,
         user_login STRING,
         user_email STRING
     )
-''')
+'''
 
 # Insert data into the table
-for entry in data:
-    cursor.execute('''
+insert_users_sql = '''
         INSERT INTO users (user_id, user_name, user_login, user_email)
         VALUES (?, ?, ?, ?)
-    ''', (entry['user_id'], entry['user_name'], entry['user_login'], entry['user_email']))
+    '''
 
-# Commit and close
-conn.commit()
+query_users_sql = 'SELECT COUNT(user_id), COUNT (DISTINCT user_id) FROM users'
 
-# Query the data
-cursor.execute('SELECT COUNT(user_id), COUNT (DISTINCT user_id) FROM users')
-rows = cursor.fetchall()
+# Connect to SQLite and create table
+conn, cursor = connect_to_db('main.db')
+cursor.execute('DROP TABLE IF EXISTS users')
+create_table(cursor, create_users_table_sql)
 
-conn.close()
+# Insert data into the table
+insert_data(cursor, insert_users_sql, [
+    (
+        entry['user_id'], entry['user_name'], entry['user_login'], entry['user_email']
+    )
+    for entry in data
+])
+
+# Query and print results
+rows = query_data(cursor, query_users_sql)
+close_connection(conn)
 
 total_users, unique_users = rows[0]
 
 print(f"### load_users.py executed successfully. {total_users} users were loaded into the table, with {unique_users} unique ids. ###")
-
-
-

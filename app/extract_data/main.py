26f577461ac7586fbbfcb09a25c9f626d8792f84
Michelle N. Fitzpatrick <michgithub99@gmail.com>
2024-09-05 01:48:43 -0400

    create main.py to execute data load functions

---
+++ app/extract_data/main.py
@@ -0,0 +1,29 @@
+from extract_data_functions import fetch_data, extract_commits, extract_issues, write_json_file
+
+def main():
+    owner = "psf" 
+    repository = "requests" 
+
+    # Fetch commits
+    commits = fetch_data(owner, repository, "commits")
+    if commits:
+        # Extract commit and user data
+        commit_data, user_data = extract_commits(commits)
+        
+        # Write commit and user data to JSON files
+        write_json_file("commits.json", commit_data)
+        write_json_file("users.json", user_data)
+
+    # Fetch issues
+    issues = fetch_data(owner, repository, "issues")
+    if issues:
+        # Extract issue data
+        issue_data = extract_issues(issues)
+        
+        # Write issue data to a JSON file
+        write_json_file("issues.json", issue_data)
+
+    print("### Main Data Extraction Split Ran Successfully ###")
+
+if __name__ == "__main__":
+    main()
